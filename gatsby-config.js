c8729a1441c00e28563cdc1f9753d51769122989
Jose <jozylopez27@gmail.com>
2020-07-06 17:40:06 -0500

    third

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Jose Lopez Developer Portfolio`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Portfolio`,
     author: `@gatsbyjs`,
   },
   plugins: [
